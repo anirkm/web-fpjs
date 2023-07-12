bef3611b30adf06a7b6b8b36e872d5e3e636ecc7
Yatsuki <yatsuki@email.moe>
2023-07-12 16:51:57 +0100

    d

--- src/app/api/appeal/user/route.ts
+++ src/app/api/appeal/user/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import discordApi from "@/lib/discord";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { captureException } from "@sentry/nextjs";
 
 export async function GET(req: NextRequest) {
